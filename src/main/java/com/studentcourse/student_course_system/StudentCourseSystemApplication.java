afa209f5dd40a1a502bc92d528e26194c58a8e0b
hamouzei <hamuzy07@gmail.com>
2026-03-06 00:26:05 +0300

    project initialized

---
+++ src/main/java/com/studentcourse/student_course_system/StudentCourseSystemApplication.java
@@ -0,0 +1,13 @@
+package com.studentcourse.student_course_system;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class StudentCourseSystemApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(StudentCourseSystemApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/studentcourse/student_course_system/StudentCourseSystemApplicationTests.java
@@ -0,0 +1,13 @@
+package com.studentcourse.student_course_system;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class StudentCourseSystemApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
